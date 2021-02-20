5445ee8c93bde07897c31ab4c50ef4b30e362784
MRXY001 <wxy19980615@gmail.com>
2021-02-20 11:12:42 +0800

    添加拉出来、耷拉下垂两阶段动画

--- fourowidget.cpp
+++ fourowidget.cpp
@@ -1,16 +1,50 @@
 #include <QPainter>
 #include <QPainterPath>
 #include <QPropertyAnimation>
+#include <QSequentialAnimationGroup>
+#include <QKeyEvent>
+#include <QDebug>
+#include <math.h>
 #include "fourowidget.h"
 
 FourOWidget::FourOWidget(QWidget *parent) : QWidget(parent)
 {
+    this->setFocusPolicy(Qt::StrongFocus);
+    this->setFocus();
+}
 
+void FourOWidget::startAnimation()
+{
+    pullOutProgress = droopProgress = downfallProgress = xRotateProgress = 0;
+
+    QSequentialAnimationGroup* group = new QSequentialAnimationGroup(this);
+    connect(group, SIGNAL(finished()), group, SLOT(deleteLater()));
+
+    {
+        QPropertyAnimation* ani = new QPropertyAnimation(this, "pullOutProgress");
+        ani->setStartValue(0);
+        ani->setEndValue(100);
+        ani->setDuration(500);
+        ani->setEasingCurve(QEasingCurve::OutCubic);
+        group->addAnimation(ani);
+    }
+
+    {
+        QPropertyAnimation* ani = new QPropertyAnimation(this, "droopProgress");
+        ani->setStartValue(0);
+        ani->setEndValue(100);
+        ani->setDuration(500);
+        ani->setEasingCurve(QEasingCurve::OutBounce);
+        group->addAnimation(ani);
+    }
+
+    group->start();
 }
 
 void FourOWidget::paintEvent(QPaintEvent *)
 {
     QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing, true);
 
     // 字体属性
     QFont font;
@@ -33,7 +67,7 @@ void FourOWidget::paintEvent(QPaintEvent *)
     painter.fillRect(0, statusBarHeight + appBarHeight, width(), height(), Qt::white);
 
     // 文字属性
-    double paddingLeft = (lineSpacing - fontHeight) * 3;
+    const double paddingLeft = (lineSpacing - fontHeight) * 3;
     const double widthOfN = fm.horizontalAdvance("-");
     const double widthOf4 = fm.horizontalAdvance("4");
     const double textBottom = lineSpacing;
@@ -42,35 +76,102 @@ void FourOWidget::paintEvent(QPaintEvent *)
 
     // 画固定的 4°
     painter.setPen(QPen(textColor, 2));
-    painter.drawText(QPointF(paddingLeft, textBottom), "-");
-    painter.drawText(QPointF(paddingLeft += widthOfN, textBottom), "4");
-    painter.drawText(QPointF(paddingLeft + widthOf4 * 2 / 3, textBottom - 2), "°");
+    double padding = paddingLeft;
+    painter.drawText(QPointF(padding += widthOfN, textBottom), "4");
+    painter.drawText(QPointF(padding + widthOf4 * 2 / 3, textBottom - 2), "°");
+    if (pullOutProgress && !droopProgress) // 正在憋屎，脸涨红
+    {
+        painter.save();
+        QColor c = suppressColor;
+        c.setAlpha(255 * pullOutProgress / 100);
+        painter.setPen(c);
+        painter.drawText(QPointF(padding + widthOf4 * 2 / 3, textBottom - 2), "°");
+        painter.restore();
+    }
 
 
     // 画旋转的 X
-    const double xPadding = paddingLeft;
+    const double xPadding = padding;
     QRectF xRect(xPadding, xPadding + statusBarHeight, appBarHeight - xPadding*2, appBarHeight - xPadding*2);
     painter.drawLine(xRect.topLeft(), xRect.bottomRight());
     painter.drawLine(xRect.topRight(), xRect.bottomLeft());
 
 
     // 画掉落的 -
+    const double barLen = widthOfN * 0.8;
+    const double barRight = paddingLeft + barLen + widthOfN * 0.1;
+    const double barTop = textBottom - fontHeight / 3.6; // 纵坐标，根据字体4的位置慢慢调
+    const double barLeft = barRight - barLen * pullOutProgress / 100.0;
+    if (!pullOutProgress) // 静态
+    {}
+    else if (pullOutProgress && !droopProgress) // 阶段一：拉出来
+    {
+        painter.drawLine(QPointF(barLeft, barTop), QPointF(barRight, barTop));
+    }
+    else if (droopProgress && !downfallProgress) // 阶段二：耷拉下垂
+    {
+        const double angle = 90 * droopProgress / 100;
+        const double degree = angle * 2 * PI / 360;
+        const double leftX = barRight - cos(degree) * barLen;
+        const double leftY = barTop + sin(degree) * barLen;
+        painter.drawLine(QPointF(leftX, leftY), QPointF(barRight, barTop));
+    }
+    else if (downfallProgress && !xRotateProgress) // 阶段三：坠落
+    {
 
+    }
+    else
+    {
+
+    }
 }
 
-void FourOWidget::setDropBarProgress(const int &x)
+void FourOWidget::keyPressEvent(QKeyEvent *event)
 {
-    this->dropBarProgress = x;
+    if (event->key() == Qt::Key_Space)
+    {
+        startAnimation();
+    }
+    return QWidget::keyPressEvent(event);
 }
 
-int FourOWidget::getDropBarProgress() const
+void FourOWidget::setPullOutProgress(const int &x)
 {
-    return dropBarProgress;
+    this->pullOutProgress = x;
+    update();
+}
+
+int FourOWidget::getPullOutProgress() const
+{
+    return pullOutProgress;
+}
+
+void FourOWidget::setDroopProgress(const int &x)
+{
+    this->droopProgress = x;
+    update();
+}
+
+int FourOWidget::getDroopProgress() const
+{
+    return droopProgress;
+}
+
+void FourOWidget::setDownfallProgress(const int &x)
+{
+    this->downfallProgress = x;
+    update();
+}
+
+int FourOWidget::getDownfallProgress() const
+{
+    return downfallProgress;
 }
 
 void FourOWidget::setXRotateProgress(const int &x)
 {
     this->xRotateProgress = x;
+    update();
 }
 
 int FourOWidget::getXRotateProgress() const
--- fourowidget.h
+++ fourowidget.h
@@ -4,26 +4,39 @@
 #include <QObject>
 #include <QWidget>
 
+#define PI 3.141592
+
 class FourOWidget : public QWidget
 {
     Q_OBJECT
-    Q_PROPERTY(int dropBarProgress READ getDropBarProgress WRITE setDropBarProgress)
+    Q_PROPERTY(int pullOutProgress READ getPullOutProgress WRITE setPullOutProgress)
+    Q_PROPERTY(int droopProgress READ getDroopProgress WRITE setDroopProgress)
+    Q_PROPERTY(int downfallProgress READ getDownfallProgress WRITE setDownfallProgress)
     Q_PROPERTY(int xRotateProgress READ getXRotateProgress WRITE setXRotateProgress)
 public:
     explicit FourOWidget(QWidget *parent = nullptr);
 
+    void startAnimation();
+
 protected:
     void paintEvent(QPaintEvent *) override;
+    void keyPressEvent(QKeyEvent *event) override;
 
 private:
-    void setDropBarProgress(const int& x);
-    int getDropBarProgress() const;
+    void setPullOutProgress(const int& x);
+    int getPullOutProgress() const;
+    void setDroopProgress(const int& x);
+    int getDroopProgress() const;
+    void setDownfallProgress(const int& x);
+    int getDownfallProgress() const;
     void setXRotateProgress(const int& x);
     int getXRotateProgress() const;
 
 private:
-    int dropBarProgress = 0;
-    int xRotateProgress = 0;
+    int pullOutProgress = 0;  // 一阶段：憋红脸拉屎
+    int droopProgress = 0;    // 二阶段：耷拉下垂
+    int downfallProgress = 0; // 三阶段：坠落
+    int xRotateProgress = 0;  // 四阶段：砸中X并开始旋转
 };
 
 #endif // FOUROWIDGET_H
